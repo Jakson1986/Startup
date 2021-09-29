e15c195ee5a6c3d3c9ade68d0f49af80a489b1a0
Евгений Евгений <savok310586@gmail.com>
2021-09-29 21:31:46 +0300

    lesson 12

--- js/scripts.js
+++ js/scripts.js
@@ -9,34 +9,41 @@ $(document).ready(function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    slidesPerView: 4,
-    spaceBetween: 30,
-    speed: 400,
-
    
-      
+    speed: 400,
+    spaceBetween: 30,
+    
     breakpoints: {
-       320: {
-        slidesPerView: 1,
-        spaceBetween: 20
+      320: {
+        slidesPerView: 1,        
       },
 
-
       580: {
-        slidesPerView: 2,
-        spaceBetween: 20
+        slidesPerView: 2,        
       },
 
       920: {
-        slidesPerView: 3,
-        spaceBetween: 10
+        slidesPerView: 3,        
       },
 
       1180: {
-        slidesPerView: 4,
-        spaceBetween: 30
+        slidesPerView: 4,        
       }
     }
   });
+    $(function() {
+    var tab = $('#tabs .tabs-items > div'); 
+    tab.hide().filter(':first').show(); 
+    
+
+    $('#tabs .tabs-nav a').click(function(){
+      tab.hide(); 
+      tab.filter(this.hash).show(); 
+      $('#tabs .tabs-nav a').removeClass('active');
+      $(this).addClass('active');
+      return false;
+    }).filter(':first').click(); 
+    
+    }); 
   
 })
